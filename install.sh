34d2d4a5c05cb51ab436de1ba68b7a660e48db63
Igor Eremenko <mogila1708@gmail.com>
2020-02-09 17:12:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-printf "lalalalalala";
+cd ~/
+mkdir bashtemp
+
+cd bashtemp
+git clone git@github.com:merelyigor/bash-customs.git
+
+cd ~/bashtemp/bash-customs/my-bash-scripts
+chmod 777 change_hosts.sh
+chmod 777 devilbox.sh
+chmod 777 docker-exec.sh
+chmod 777 docker-logls.sh
+chmod 777 docker-logps.sh
+chmod 777 man.sh
+
+cd ~/bashtemp/bash-customs
+mv my-bash-scripts ~/my-bash-scripts
+
+cd ~/bashtemp/bash-customs
+mv .bash_profile ~/.bash_profile
+
+cd ~/
+source ~/.bash_profile
+rm -rf bashtemp
+
+printf "Если у тебя нечего не скачалось то ты значит не имеешь права на это))
+проси чтобы твой ключь добавил в репо его создатель"
+
 
