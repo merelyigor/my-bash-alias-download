edfe4607739f3fe77e85a74672058f445a2eab84
Igor Eremenko <mogila1708@gmail.com>
2020-03-03 13:43:41 +0200

    Update install-rc.sh

--- install-rc.sh
+++ install-rc.sh
@@ -4,38 +4,38 @@
 #----------------------------------------------
 
 cd ~/
-printf "перешел в home директорию"
+printf "перешел в home директорию\n"
 rm -rf bashtemp
-printf "удалил папку bashtemp если она есть"
+printf "удалил папку bashtemp если она есть\n"
 mkdir bashtemp
-printf "создал папку bashtemp"
+printf "создал папку bashtemp\n"
 
 cd bashtemp
-printf "перешел в папку bashtemp"
+printf "перешел в папку bashtemp\n"
 git clone git@github.com:merelyigor/bash-customs.git
-printf "склонировал репозиторий (merelyigor/bash-customs.git) в папку bashtemp"
+printf "склонировал репозиторий (merelyigor/bash-customs.git) в папку bashtemp\n"
 
 cd ~/bashtemp/bash-customs
-printf "перешел в папку ~/bashtemp/bash-customs"
+printf "перешел в папку ~/bashtemp/bash-customs\n"
 cp -r my-bash-scripts ~/
-printf "скопировал папку my-bash-scripts в home дерикторию"
+printf "скопировал папку my-bash-scripts в home дерикторию\n"
 
 cd ~/
-printf "перешел в home дерикторию"
+printf "перешел в home дерикторию\n"
 chmod -R 777 my-bash-scripts/
-printf "прописал супер права для паапки my-bash-scripts/ и её содержимого"
+printf "прописал супер права для паапки my-bash-scripts/ и её содержимого\n"
 
 cd ~/bashtemp/bash-customs
-printf "перешел в папку ~/bashtemp/bash-customs"
+printf "перешел в папку ~/bashtemp/bash-customs\n"
 cp -r .bashrc ~/.bashrc
-printf "скопировал .bashrc в home дерикторию по пути ~/.bashrc"
+printf "скопировал .bashrc в home дерикторию по пути ~/.bashrc\n"
 
 cd ~/
-printf "перешел в home дерикторию"
+printf "перешел в home дерикторию\n"
 source ~/.bashrc
-printf "выполнил команду source ~/.bashrc"
+printf "выполнил команду source ~/.bashrc\n"
 rm -rf bashtemp
-printf "удалил папку ~/bashtemp/"
+printf "удалил папку ~/bashtemp/\n"
 
 printf "Если у тебя нечего не скачалось то ты значит не имеешь права на это))
 проси чтобы твой ключь добавил в репо его создатель\n"
